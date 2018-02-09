80bff8505d568a2682351a89e5606b587b90584f
juan260 <superjuan260@hotmail.com>
2018-02-09 19:51:11 +0100

    asdf

---
+++ 1/p1-ej1.lisp
@@ -0,0 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; sc-rec (x y)
+;;; Calcula la similitud coseno de un vector de forma recursiva
+;;;
+;;; INPUT: x: vector, representado como una lista
+;;; y: vector, representado como una lista
+;;;
+;;; OUTPUT: similitud coseno entre x e y
+;;;
+(defun prodEscRec (x y)
+    (if (or (null x) (null y)) 
+        nil
+     (+ 
+     )
+)
+
+
+(defun sc-rec (x y) ...)
