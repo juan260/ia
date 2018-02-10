ab5995b2c2964fa895067b3a147257b98a62138d
juan260 <superjuan260@hotmail.com>
2018-02-10 21:21:13 +0100

    asdf

--- 1/p1-ej1.lisp
+++ 1/p1-ej1.lisp
@@ -2,6 +2,9 @@
 ;;; sc-rec (x y)
 ;;; Calcula la similitud coseno de un vector de forma recursiva
 ;;;
+;;; Se asume que los dos vectores de entrada tienen la misma longitud.
+;;; La semejanza coseno entre dos vectores que son listas vacías o que son
+;;; (0 0...0) es NIL.
 ;;; INPUT: x: vector, representado como una lista
 ;;; y: vector, representado como una lista
 ;;;
@@ -20,11 +23,76 @@
     (/ (prodEscRec x y) (sqrt (* (prodEscRec x x) (prodEscRec y y)))))
 
 
-(defun 
-
 (defun prodEscMapcar (x y)
-    (if (or (null x) (null y)) 
-        0
-     (+ (* (first x) (first y)) (prodEscRec (cdr x) (cdr y)))
-     )
+    (reduce #'+ (mapcar #'* x y))
 )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; sc-mapcar (x y)
+;;; Calcula la similitud coseno de un vector usando mapcar
+;;;
+;;; Se asume que los dos vectores de entrada tienen la misma longitud.
+;;; La semejanza coseno entre dos vectores que son listas vacías o que son
+;;; (0 0...0) es NIL.
+;;; INPUT: x: vector, representado como una lista
+;;; y: vector, representado como una lista
+;;;
+;;; OUTPUT: similitud coseno entre x e y
+;;;
+(defun sc-mapcar (x y) 
+;;TODO: Falta comprobar la division por 0
+    (/ (prodEscMapcar x y) (sqrt (* (prodEscMapcar x x) (prodEscMapcar y y)))))
+    
+    
+    
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; sc-conf (cat vs conf)
+;;; Devuelve aquellos vectores similares a una categoria
+;;; INPUT: cat: vector que representa a una categoría, representado como una lista
+;;; vs: vector de vectores
+;;; conf: Nivel de confianza
+;;; OUTPUT: Vectores cuya similitud con respecto a la categoría es superior al
+;;; nivel de confianza, ordenados
+(defun select (conf cat vs)
+	(if (>= (sc-rec (car vs) cat) conf) 
+		vs
+		(select conf cat (cdr vs)))) 
+
+(defun sc-conf (cat vs conf) 
+	(select conf cat (sort (copy-list vs)
+		#'(lambda (x y) (< (sc-rec x cat) (sc-rec y cat))))
+	
+))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; sc-classifier (cats texts func)
+;; Clasifica a los textos en categorías.
+;;;
+;;; INPUT: cats: vector de vectores, representado como una lista de listas
+;;; texts: vector de vectores, representado como una lista de listas
+;;; func: función para evaluar la similitud coseno
+;;; OUTPUT: Pares identificador de categoría con resultado de similitud coseno
+;;;
+
+(defun mincadr (x y) 
+	(if(< (second x) (second y)) x y))
+
+(defun classify (cats text func)
+	(if (null cats) '(-2 0)
+		(mincadr (list (caadr cats) (func (cdr text) (cdar cats))) 
+				(classify (cdr cats) text func))))
+	
+	
+(defun sc-classifier (cats texts func)
+	(if (null texts) nil 
+		(cons (classify cats (car texts) func)	
+			(sc-classifier cats (cdr texts) func)))
+
+)
+
+
+
+
