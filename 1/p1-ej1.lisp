12e2b9949fa800403a20b2f1a9ab820f1f4db28d
juan260 <superjuan260@hotmail.com>
2018-02-11 00:06:07 +0100

    asdf

--- 1/p1-ej1.lisp
+++ 1/p1-ej1.lisp
@@ -77,12 +77,12 @@
 ;;; OUTPUT: Pares identificador de categoría con resultado de similitud coseno
 ;;;
 
-(defun mincadr (x y) 
-	(if(< (second x) (second y)) x y))
+(defun maxcadr (x y) 
+	(if(> (second x) (second y)) x y))
 
 (defun classify (cats text func)
 	(if (null cats) '(-2 0)
-		(mincadr (list (caadr cats) (func (cdr text) (cdar cats))) 
+		(maxcadr (list (caar cats) (funcall func (cdr text) (cdar cats))) 
 				(classify (cdr cats) text func))))
 	
 	
@@ -94,5 +94,38 @@
 )
 
 
+;;;; RENDIMIENTOS:
+;;;;[28]> (time (sc-classifier cats texts #'sc-rec))
+;;;;Real time: 1.54E-4 sec.
+;;;;Run time: 0.0 sec.
+;;;;Space: 192 Bytes
+;;;;((1 1) (2 0.8017837))
+
+;;;; RENDIMIENTOS
+;;;;[29]> (time (sc-classifier cats texts #'sc-mapcar))
+;;;;Real time: 1.35E-4 sec.
+;;;;Run time: 0.0 sec.
+;;;;Space: 768 Bytes
+;;;;((1 1) (2 0.8017837))
+
+
+;;;; RENDIMIENTOS CON LOS DATOS DEL ENUNCIADO
+
+(setf cats '((1 43 23 12) (2 33 54 24)))
+(setf texts '((1 3 22 134) (2 43 26 58)))
+(time (sc-classifier cats texts #'sc-rec))
+(time (sc-classifier cats texts #'sc-mapcar))
+
+;;;;[32]> (time (sc-classifier cats texts #'sc-rec))
+;;;;Real time: 1.51E-4 sec.
+;;;;Run time: 0.0 sec.
+;;;;Space: 160 Bytes
+;;;;((2 0.48981872) (1 0.8155509))
+
+;;;;[33]> (time (sc-classifier cats texts #'sc-mapcar))
+;;;;Real time: 7.2E-5 sec.
+;;;;Run time: 0.0 sec.
+;;;;Space: 736 Bytes
+;;;;((2 0.48981872) (1 0.8155509))
 
 
