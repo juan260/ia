5be2dc4850c6a665ff8410905de74f6617a02cf6
luciaasen <luciaasen@gmail.com>
2018-02-12 01:15:21 +0100

    puedo morirme a bit

---
+++ 1/p1-ej2.lisp
@@ -0,0 +1,118 @@
+;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 2.1 ;;;;;;;;;;;;;;;;;;
+
+;; Finds a root of f between the points a and b using bisection.
+;;
+;; If f(a)f(b)>=0 there is no guarantee that there will be a root in the
+;; interval, and the function will return NIL.
+;; INPUT:
+;; f: function of a single real parameter with real values whose root we want to find
+;; a: lower extremum of the interval in which we search for the root
+;; b: b>a upper extremum of the interval in which we search for the root
+;; tol: tolerance for the stopping criterion: if b-a < tol the function returns
+;; (a+b)/2 as a solution.
+;; OUTPUT: Root of the function
+
+(defun bisect (f a b tol)
+    (let ((medio (/ (+ a b) 2)))
+        (cond ((< 0 (* (funcall f a) (funcall f b))) NIL )
+              ((= 0 (funcall f a)) a)
+              ((= 0 (funcall f b)) b)
+              ((> tol (- b a)) medio)
+              ((>= 0 (* (funcall f a) (funcall f medio))) (bisect f a medio tol))
+              ((>= 0 (* (funcall f b) (funcall f medio))) (bisect f medio b tol)))))
+              
+
+;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 2.2 ;;;;;;;;;;;;;;;;;;
+
+;; Funcion auxiliar
+;; Igual que la funcion allroot, pero devuelve una lista que contiene las raices
+;; intercaladas con NILs
+;;
+;;
+;; INPUT:
+;; f: function of a single real parameter with real values whose root
+;; we want to find
+;; lst: ordered list of real values (lst[i] < lst[i+1])
+;; tol: tolerance for the stopping criterion: if b-a < tol the function
+;; returns (a+b)/2 as a solution.
+;;
+;; Whenever sgn(f(lst[i])) != sgn(f(lst[i+1])) this function looks for a
+;; root in the corresponding interval.
+;;
+;; OUTPUT:
+;; A list o real values containing the roots of the function in the
+;; given sub-intervals
+
+(defun nils-n-root (f lst tol)
+	(unless (null (rest lst)) 
+		(cons (bisect f (first lst) (first (rest lst)) tol) (nils-n-root f (rest lst) tol ))))
+
+;; Finds all the roots that are located between consecutive values of a list
+;; of values
+;;
+;; INPUT:
+;; f: function of a single real parameter with real values whose root
+;; we want to find
+;; lst: ordered list of real values (lst[i] < lst[i+1])
+;; tol: tolerance for the stopping criterion: if b-a < tol the function
+;; returns (a+b)/2 as a solution.
+;;
+;; Whenever sgn(f(lst[i])) != sgn(f(lst[i+1])) this function looks for a
+;; root in the corresponding interval.
+;;
+;; OUTPUT:
+;; A list o real values containing the roots of the function in the
+;; given sub-intervals
+		
+(defun allroot (f lst tol)
+	(remove nil (nils-n-root f lst tol)))
+
+;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 2.3 ;;;;;;;;;;;;;;;;;;
+
+;; Funcion auxiliar
+;; Igual que la funcion allind, pero devuelve una lista que contiene las raices
+;; intercaladas con NILs
+;; 
+;; INPUT:
+;; f: function of a single real parameter with real values whose root we want to find
+;; a: lower extremum of the interval in which we search for the root
+;; b: b>a upper extremum of the interval in which we search for the root
+;; N: Exponent of the number of intervals in which [a,b] is to be divided:
+;; [a,b] is divided into 2^N intervals
+;; tol: tolerance for the stopping criterion: if b-a < tol the function
+;; returns (a+b)/2 as a solution.
+;;
+;; The interval (a,b) is divided in intervals (x[i], x[i+i]) with
+;; x[i]= a + i*dlt; a root is sought in each interval, and all the roots
+;; thus found are assembled into a list that is returned.
+;;
+;; OUTPUT: List with all the found roots.
+
+(defun  introot (f incr fin actual tol)
+	(unless (<= fin actual)
+		(cons (bisect f actual (+ actual incr) tol) 
+			  (introot f incr fin (+ actual incr) tol))))
+			  
+
+;; Divides an interval up to a specified length and find all the roots of
+;; the function f in the intervals thus obtained.
+;; INPUT:
+;; f: function of a single real parameter with real values whose root we want to find
+;; a: lower extremum of the interval in which we search for the root
+;; b: b>a upper extremum of the interval in which we search for the root
+;; N: Exponent of the number of intervals in which [a,b] is to be divided:
+;; [a,b] is divided into 2^N intervals
+;; tol: tolerance for the stopping criterion: if b-a < tol the function
+;; returns (a+b)/2 as a solution.
+;;
+;; The interval (a,b) is divided in intervals (x[i], x[i+i]) with
+;; x[i]= a + i*dlt; a root is sought in each interval, and all the roots
+;; thus found are assembled into a list that is returned.
+;;
+;; OUTPUT: List with all the found roots.
+
+(defun allind (f a b N tol)
+	(remove nil (introot f (/ (- b a) (expt 2 N)) b a tol)))
+
+
+
---
+++ 1/p1-ej3.lisp
@@ -0,0 +1,60 @@
+;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 3.1 ;;;;;;;;;;;;;;;;;;
+
+;; Combina un elto dado con todos los eltos de una lista
+;;
+;; INPUT:
+;; elt: elemento a combinar con la lista
+;; lst: llista quue sera combinada con el elemnto
+;;
+;; OUTPUT: lista que contenga la combinacion
+
+(defun combine-elt-lst (elt lst)
+	(unless (or (null lst) (null elt))
+		(cons (list elt (first lst))
+			  (combine-elt-lst elt (rest lst)) )))
+
+;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 3.2 ;;;;;;;;;;;;;;;;;;
+
+;; Funcion que devuelve el producto cartesiano de dos listas
+;;
+;; INPUT:
+;; lst1, lst2 : listas a combinar
+;;
+;; OUTPUT: lista que contenga la combinacion
+
+(defun combine-lst-lst (lst1 lst2)
+	(unless (or (null lst1) (null lst2))
+		(append (combine-elt-lst (first lst1) lst2)
+			  (combine-lst-lst (rest lst1) lst2))))
+			  
+;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 3.3 ;;;;;;;;;;;;;;;;;;
+
+;; Función que calcula todas las posibles disposiciones de elementos
+;; pertenecientes a N listas de forma que en cada disposición aparezca únicamente
+;; un elemento de cada lista
+;;
+;; INPUT:
+;; lstolsts : listas a combinar
+;;
+;; OUTPUT: lista que contenga todas las combinaciones
+
+(defun combine-list-of-lsts (lstolsts)
+	(if (atom (first lstolsts)) 
+		(if (null (rest (rest lstolsts)))
+			;; Caso (a (+ -)) -> combina a con (+ -)
+			(combine-elt-lst (first lstolsts) (rest lstolsts) )
+			;; Caso (a (+ -) (1 2)) -> combina a con el result de combinar ((+ -)(1 2))
+			(combine-elt-lst (first lstolsts) 
+							 (combine-list-of-lsts (rest lstolsts))
+			)
+		)
+		;; Caso ((a b c) (+ -) (1 2)) -> recursion pura
+		(append (combine-list-of-lsts (list (first (first lstolsts)) (rest lstolsts)))
+				(combine-list-of-lsts (list (rest (first lstolsts)) (rest lstolsts)))
+		)
+	)
+)
+
+
+
+
