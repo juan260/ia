1bd9846851bdb936b22031b89ff261d802e5526a
luciaasen <luciaasen@gmail.com>
2018-03-05 21:54:52 +0100

    qwdwrg

--- 1/p1-ej2.lisp
+++ 1/p1-ej2.lisp
@@ -22,12 +22,25 @@
               ((>= 0 (* (funcall f b) (funcall f medio))) (bisect f medio b tol)))))
               
 
-;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 2.2 ;;;;;;;;;;;;;;;;;;
+ (bisect #'(lambda(x) (sin (* 6.26 x))) 0.1 0.7 0.001)
+;; 0.5016602
+(bisect #'(lambda(x) (sin (* 6.26 x))) 0.0 0.7 0.001)
+;; 0.0
+(bisect #'(lambda(x) (sin (* 6.26 x))) 1.1 1.5 0.001)
+;; NIL
+(bisect #'(lambda(x) (sin (* 6.26 x))) 1.1 0.6 0.0000001)
+;; 0.85
+(bisect #'(lambda(x) (sin (* 6.26 x))) 1.1 0.7 0.0000001)
+;; 0.9
+(bisect #'(lambda(x) (sin (* 6.26 x))) 1.1 0.75 0.000000001)
+;; 0.925
 
+
+;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 2.2 ;;;;;;;;;;;;;;;;;;
  
 ;; Usamos esta funcion auxiliar, que se comporta como bisect pero
-;; devuelve las raíces en una lista, para evitar tener que llamar dos veces
-;; a la función bisect con cada iteración de mapcan. 
+;; devuelve las raices en una lista, para evitar tener que llamar dos veces
+;; a la funcion bisect con cada iteracion de mapcan. 
 
 (defun bisect-aux (f a b tol)
   (unless (or (null a) (null b))
@@ -62,30 +75,20 @@
             lst
             (rest lst)))) 
 
+(allroot #'(lambda(x) (sin (* 6.28 x))) '(0.25 0.75 1.25 1.75 2.25) 0.0001)
+;; (0.50027466 1.0005188 1.5007629 2.001007)
+(allroot #'(lambda(x) (sin (* 6.28 x))) '(0.25 0.9 0.75 1.25 1.75 2.25) 0.0001)
+;; (0.5002166 1.0005188 1.5007629 2.001007)
+
 ;;;;;;;;;;;;;;;;;;;;;;; EJERCICIO 2.3 ;;;;;;;;;;;;;;;;;;
 
 ;; Funcion auxiliar
-;; Igual que la funcion allind, pero devuelve una lista que contiene las raices
-;; intercaladas con NILs
+;; Busca raices en intervalos de tamanio incr en (actual, fin)
 ;; 
-;; INPUT:
-;; f: function of a single real parameter with real values whose root we want to find
-;; a: lower extremum of the interval in which we search for the root
-;; b: b>a upper extremum of the interval in which we search for the root
-;; N: Exponent of the number of intervals in which [a,b] is to be divided:
-;; [a,b] is divided into 2^N intervals
-;; tol: tolerance for the stopping criterion: if b-a < tol the function
-;; returns (a+b)/2 as a solution.
-;;
-;; The interval (a,b) is divided in intervals (x[i], x[i+i]) with
-;; x[i]= a + i*dlt; a root is sought in each interval, and all the roots
-;; thus found are assembled into a list that is returned.
-;;
-;; OUTPUT: List with all the found roots.
 
 (defun  introot (f incr fin actual tol)
     (unless (<= fin actual)
-        (cons (bisect f actual (+ actual incr) tol) 
+        (union (bisect-aux f actual (+ actual incr) tol) 
               (introot f incr fin (+ actual incr) tol))))
               
 
@@ -107,7 +110,12 @@
 ;; OUTPUT: List with all the found roots.
 
 (defun allind (f a b N tol)
-    (remove nil (introot f (/ (- b a) (expt 2 N)) b a tol)))
+    (introot f (/ (- b a) (expt 2 N)) b a tol))
 
 
+;; EXAMPLES
+(allind #'(lambda(x) (sin (* 6.28 x))) 0.1 2.25 1 0.0001)
+;; NIL
+(allind #'(lambda(x) (sin (* 6.28 x))) 0.1 2.25 2 0.0001)
+;; (0.50027096 1.000503 1.5007349 2.0010324)
 
