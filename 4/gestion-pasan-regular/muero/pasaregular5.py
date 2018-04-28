44346bcde7221b5cde65da11f6442f0fd011a28e
luciaasen <luciaasen@gmail.com>
2018-04-28 06:39:26 +0200

    muchos

---
+++ 4/gestion-pasan-regular/muero/pasaregular5.py
@@ -0,0 +1,50 @@
+import random
+import itertools
+import subprocess
+import sys, os
+
+def main():
+    
+    outputName = 'salida_' + sys.argv[1]
+    outputFile = open(outputName, 'w')
+    outputNamekk = 'salida_kk'
+    outputFilekk = open(outputNamekk, 'w')
+    
+    perm = []
+    results = ''
+    resultskk = ''
+    i = 0
+    j = 0
+    k = 0
+    while 1:    
+        perm = [2*random.randint(1, 30) for i in range(12)]
+        #String de llamada al script
+        args = '(defvar *ponderations* \'((' + str(perm[0]) + ' ' + str(perm[1]) + ' ' + str(perm[2]) +  ' ' + str(perm[3]) + ' ' + str(perm[4]) + ' ' + str(perm[5]) + ')(' + str(perm[6]) + ' ' + str(perm[7]) + ' ' + str(perm[8]) + ' ' +  str(perm[9]) + ' ' + str(perm[10]) + ' ' + str(perm[11]) + ')))\t\t'        
+        #Llamamos al script y, si pasa al regular, guardamos las ponderaciones        
+        res = subprocess.run(['./CreateAndExecute2.sh', str(perm[0]), str(perm[1]), str(perm[2]), str(perm[3]), str(perm[4]), str(perm[5]), str(perm[6]), str(perm[7]), str(perm[8]), str(perm[9]), str(perm[10]), str(perm[11])], stdout=subprocess.PIPE)
+        perc = res.stdout.decode('utf-8')
+        results += args
+        results += perc
+        if k%100 == 0:
+            print(k)
+            outputFile.write(results)
+            results = ''
+#        if pasa == 'PASA\n':      
+#            results += args
+#        
+#            if i%50 == 0:
+#                outputFile.write(results)
+#                results = ''
+#            i += 1
+#        else:
+#            resultskk += args
+#            if j%50 == 0:
+#                outputFilekk.write(resultskk)
+#                resultskk = ''
+#            j += 1
+        k += 1   
+    outputFile.write(results)
+    
+main()
+
+
