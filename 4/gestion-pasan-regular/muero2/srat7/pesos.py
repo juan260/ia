c35c17c5d8b0680679b336c6fbf1440038ede62d
juan260 <superjuan260@hotmail.com>
2018-05-02 18:59:39 +0200

    asdf

--- 4/gestion-pasan-regular/muero2/srat7/pesos.py
+++ 4/gestion-pasan-regular/muero2/srat7/pesos.py
@@ -4,27 +4,8 @@ import subprocess
 import sys, os, random
 import numpy as np
 
-def add(l, count, line):
-    if line in list(el[1] for el in l):
-        return
-    
-    for i in range(20):
-        if l[i][0] < count:
-            l[i] = (count, line)
-            print(l)
-            return
-
-def readyPlayerSetPerm(perm, script):
-    
-    subprocess.run([script, str(perm[0]), str(perm[1]), str(perm[2]), str(perm[3]), str(perm[4]), \
-    str(perm[5]), str(perm[6]), str(perm[7]), str(perm[8]), str(perm[9]), str(perm[10]), str(perm[11]), \
-    str(perm[12]), str(perm[13]), str(perm[14]), str(perm[15]), str(perm[16]), str(perm[17]), \
-    str(perm[17]), str(perm[18]), str(perm[19]), str(perm[20])], stdout=subprocess.PIPE)
-    
-def runGame():
-    res = subprocess.run(['./StartGame.sh'], stdout=subprocess.PIPE)
-    return res.stdout.decode('utf-8')
-
+# Recibe una lista (l) de tuplas y añade la tupla (count, line) manteniendola ordenada 
+# descendentemente con respecto al primer elemento y sin dejar que supere el tamaño 30
 def add(l, count, line):
     if line in list(el[1] for el in l):
         return
@@ -34,7 +15,22 @@ def add(l, count, line):
             l[i] = (count, line)
             print(l)
             return
+            
+# Recibe una lista de 21 argumentos para pasarselos al script y generar el jugador. El script que recibe es
+# CreatePlayer1.sh o CreatePlayer2.sh
+def readyPlayerSetPerm(perm, script):
+    
+    subprocess.run([script, str(perm[0]), str(perm[1]), str(perm[2]), str(perm[3]), str(perm[4]), \
+    str(perm[5]), str(perm[6]), str(perm[7]), str(perm[8]), str(perm[9]), str(perm[10]), str(perm[11]), \
+    str(perm[12]), str(perm[13]), str(perm[14]), str(perm[15]), str(perm[16]), str(perm[17]), \
+    str(perm[17]), str(perm[18]), str(perm[19]), str(perm[20])], stdout=subprocess.PIPE)
+  
+# Ejecuta la partida  
+def runGame():
+    res = subprocess.run(['./StartGame.sh'], stdout=subprocess.PIPE)
+    return res.stdout.decode('utf-8')
 
+# Recibe los argumentos para crear al jugador y le introduce un factor aleatorio
 def randomizeLine(line):
     splitted = line.split(" ")
     ret = list()
@@ -52,12 +48,13 @@ def main():
     infile.close()
     posibilidades = list()
     results = list()
-    while True:
-        for line1 in lines:
-            for i in np.arange(-1, 1, 0.1):
-                posibilidades.append(randomizeLine(line1) + ' ' + str(i))
+    # 
     
-        print(posibilidades[0])
+    for line1 in lines:
+        for i in np.arange(-1, 1, 0.1):
+            posibilidades.append(randomizeLine(line1) + ' ' + str(i))
+    
+    while True:
         for p1 in posibilidades:
             readyPlayerSetPerm(p1.split(" "), './CreatePlayer1.sh')
             res = 0.0
@@ -72,6 +69,11 @@ def main():
         for el in l:
             outputFile.write(str(el[1]) + '\t\t' + el[0] + '\n')
             print(el)
+        
+        posibilidades = list()
+        for el in l:
+            for i in np.arange(-1, 1, 0.1):
+                posibilidades.append(randomizeLine(' '.join(el[1])) + ' ' + str(i))
     
 main()
 
